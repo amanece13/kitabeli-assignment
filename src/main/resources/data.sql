fe579c4e8cdb69833c730f016f3a41f32ad7bfb6
TechiAman <abhishek.aman874@gmail.com>
2021-06-17 22:59:59 +0530

    added sample sql data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,19 +1,27 @@
-insert into product_category (id, name, description) values(1, 'footwear', 'adidas casuals');
-insert into product_category (id, name, description) values(2, 'footwear', 'nike casuals');
-insert into product_category (id, name, description) values(3, 'footwear', 'puma casuals');
-insert into product_category (id, name, description) values(4, 'footwear', 'reebok casuals');
-insert into product_category (id, name, description) values(5, 'tshirt', 'adidas casuals');
-insert into product_category (id, name, description) values(6, 'shirt', 'nike casuals');
-insert into product_category (id, name, description) values(7, 'hoodie', 'puma casuals');
-insert into product_category (id, name, description) values(8, 'floaters', 'reebok casuals');
+
+insert into product_category (id, name, description) values(1, 'beauty', 'Beauty Products');
+insert into product_category (id, name, description) values(2, 'books', 'Readers digest');
+insert into product_category (id, name, description) values(3, 'electronics', 'Electronic appliances');
+insert into product_category (id, name, description) values(4, 'furniture', 'Furniture at home and work');
+insert into product_category (id, name, description) values(5, 'kitchen', 'Accessories for your kitchen');
+insert into product_category (id, name, description) values(6, 'jewellery', 'Jewellery for every ocassion');
+insert into product_category (id, name, description) values(7, 'music', 'Music for every mood');
+insert into product_category (id, name, description) values(8, 'watches', 'Cool wrist watches');
+insert into product_category (id, name, description) values(9, 'grocery', 'Everything required for your hunger');
+insert into product_category (id, name, description) values(10, 'footwear', 'Stare at the shoes');
+insert into product_category (id, name, description) values(11, 'clothing', 'Rich and comfortable apparels');
+insert into product_category (id, name, description) values(12, 'baby', 'Baby products');
 
 
-
-insert into product_inventory (id, quantity) values(1, 10);
-insert into product_inventory (id, quantity) values(2, 10);
-insert into product_inventory (id, quantity) values(3, 10);
-insert into product_inventory (id, quantity) values(4, 10);
-insert into product_inventory (id, quantity) values(5, 10);
-insert into product_inventory (id, quantity) values(6, 10);
-insert into product_inventory (id, quantity) values(7, 10);
-insert into product_inventory (id, quantity) values(8, 10);
+insert into product_inventory (id, quantity) values(1, 100);
+insert into product_inventory (id, quantity) values(2, 160);
+insert into product_inventory (id, quantity) values(3, 50);
+insert into product_inventory (id, quantity) values(4, 76);
+insert into product_inventory (id, quantity) values(5, 20);
+insert into product_inventory (id, quantity) values(6, 89);
+insert into product_inventory (id, quantity) values(7, 65);
+insert into product_inventory (id, quantity) values(8, 101);
+insert into product_inventory (id, quantity) values(9, 90);
+insert into product_inventory (id, quantity) values(10, 45);
+insert into product_inventory (id, quantity) values(11, 0);
+insert into product_inventory (id, quantity) values(12, 23);
